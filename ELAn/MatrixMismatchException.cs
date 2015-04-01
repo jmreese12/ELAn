ddd06ba567f783c4dc080dabdb89a804a376c1d2
Jason Reese <jq0536@cs.ship.edu>
2015-04-01 14:06:12 -0400

    Test

---
+++ ELAn/MatrixMismatchException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELAn
+{
+    public class MatrixMismatchException : Exception
+    {
+       public MatrixMismatchException(string message) : base(message)
+       {
+
+       }
+    }
+}
